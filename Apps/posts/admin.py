2199c2bb787d207e60a7037f7ef36de3b69dd3df
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-06-04 18:19:49 -0700

    modified admin models

--- Apps/posts/admin.py
+++ Apps/posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Like
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Like, LikeAdmin)
--- Apps/users/admin.py
+++ Apps/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import User, Follows
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+class FollowsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Follows, FollowsAdmin)
