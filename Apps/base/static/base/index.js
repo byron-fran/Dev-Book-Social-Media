198ea447b0743098d3669cb296bbf6922b63fd72
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-06-12 08:32:00 -0700

    fix sidebar js

---
+++ Apps/base/static/base/index.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const menuButton = document.querySelector('#menu');
+    const sidebar = document.querySelector('#sidebar');
+    const body = document.body;
+
+    const hideSidebar = () => {
+        sidebar.classList.remove('flex', 'absolute', 'h-screen', 'z-50', 'bg-white', 'top-0', 'w-[80%]');
+        sidebar.classList.add('hidden');
+        body.classList.remove('sidebar-open');
+    };
+
+    menuButton.addEventListener('click', (e) => {
+
+        e.stopPropagation();
+        sidebar.classList.add('flex', 'absolute', 'h-screen', 'z-50', 'bg-white', 'top-0', 'w-[80%]');
+        sidebar.classList.remove('hidden');
+        body.classList.add('sidebar-open');
+
+    });
+
+    window.addEventListener('click', (e) => {
+        if (!sidebar.contains(e.target) && !menuButton.contains(e.target)) {
+            hideSidebar();
+        }
+    });
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth >= 768) {
+            hideSidebar();
+        }
+    });
+});
--- Apps/posts/managers.py
+++ Apps/posts/managers.py
@@ -3,5 +3,5 @@ from django.db.models import Manager
 class PostManager(Manager):
     
     def list_posts(self):
-        return self.all().order_by('created')
+        return self.all().order_by('-created')
     
